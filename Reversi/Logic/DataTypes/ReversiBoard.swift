7950a3d25ba94a963ca41aace212c4e358c11b5e
KuroruriShield <23126699+KuroruriShield@users.noreply.github.com>
2020-04-24 21:33:15 +0900

    define ReversiBoard type

---
+++ Reversi/Logic/DataTypes/ReversiBoard.swift
@@ -0,0 +1,11 @@
+//
+//  ReversiBoard.swift
+//  Reversi
+//
+//  Created by Kuroruri on 2020/04/24.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+import Foundation
+
+typealias ReversiBoard = [Disk?]
