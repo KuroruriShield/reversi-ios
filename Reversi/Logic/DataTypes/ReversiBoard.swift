093aeba3eb7b3631ad5177a2de94b58008982dbf
KuroruriShield <23126699+KuroruriShield@users.noreply.github.com>
2020-04-24 21:47:36 +0900

    add diskAt func

--- Reversi/Logic/DataTypes/ReversiBoard.swift
+++ Reversi/Logic/DataTypes/ReversiBoard.swift
@@ -9,3 +9,23 @@
 import Foundation
 
 typealias ReversiBoard = [Disk?]
+
+public extension ReversiBoard {
+    /// 盤の幅（ `8` ）を表します。
+    var width: Int { 8 }
+    
+    /// 盤の高さ（ `8` ）を返します。
+    var height: Int { 8 }
+    
+    /// 盤のセルの `x` の範囲（ `0 ..< 8` ）を返します。
+    var xRange: Range<Int> { 0 ..< width }
+    
+    /// 盤のセルの `y` の範囲（ `0 ..< 8` ）を返します。
+    var yRange: Range<Int> { 0 ..< height }
+    
+    func diskAt(x: Int, y: Int) -> Disk? {
+        guard xRange.contains(x) && yRange.contains(y) else { return nil }
+        return self[y * width + x]
+    }
+
+}
